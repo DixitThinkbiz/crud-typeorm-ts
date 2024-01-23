6b5d36eb8ec00894376e86db0cb9068e4e43d841
DixitThinkbiz <dixitjayswal@vebuin.com>
2024-01-23 12:24:58 +0530

    Final commit

--- src/infrastructure/webserver/express/index.ts
+++ src/infrastructure/webserver/express/index.ts
@@ -4,6 +4,10 @@ import dotenv from 'dotenv';
 import { AppDataSource } from "../../orm/typeorm/config/ormconfig";
 import { router } from "../../../interface/routes/dummy_routes";
 import "reflect-metadata"
+import swaggerUi from 'swagger-ui-express';
+import swaggerJSDoc from "swagger-jsdoc";
+const swaggerDocument = require('../../../../swagger/swagger.json');
+
 // Load environment variables from the .env file
 dotenv.config();
 
@@ -13,7 +17,6 @@ app.use(express.json());
 
 // Set the port for the server
 const port = process.env.PORT;
-
 // Define a basic route for testing
 app.get("/", (req, res) => {
   res.send("Working");
@@ -25,7 +28,12 @@ app.use('/user', router);
 // Initialize the AppDataSource and start the server
 AppDataSource.initialize().then(() => {
   console.log("Database connection successful");
+
+  
+  app.use('/user-api', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+  
   app.listen(port, () => {
+
     console.log(`Server is running on http://localhost:${port}`);
   });
 }).catch((e) => {
--- src/interface/routes/utils.ts
+++ src/interface/routes/utils.ts
@@ -2,6 +2,9 @@
 import { NextFunction, Request, Response } from "express";
 import { ObjectSchema } from "joi";
 import { constants } from "../../infrastructure/config/constant";
+import { Application } from 'express';
+
+
 
 export const validateDummyData = (schema:ObjectSchema
   ) => (req:Request, res:Response, next: NextFunction) => {
