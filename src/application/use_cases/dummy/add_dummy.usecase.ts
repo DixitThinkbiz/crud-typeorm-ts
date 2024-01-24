6b415101a7c49dcb3624bb8f72cd83e530e44793
DixitThinkbiz <156079281+DixitThinkbiz@users.noreply.github.com>
2024-01-24 14:46:16 +0530

    Final Commit (#3)

--- src/application/use_cases/dummy/add_dummy.usecase.ts
+++ src/application/use_cases/dummy/add_dummy.usecase.ts
@@ -8,12 +8,10 @@ import { DummyRepositoryPort } from "../../port/repositories/dummy_repo.port";
 export const addDummyUsecase = async (DummyRepo:DummyRepositoryPort,dummyData: Dummy,t:EntityManager) => {
     // Check if a dummy with the specified email already exists
     const selectedDummy : Dummy= await DummyRepo.checkDummyEmailExist(dummyData.email,t);
-
     // If a dummy with the same email exists, throw an error
     if (selectedDummy) {
         throw new Error(constants.ERROR_MESSAGE.USER_ALREADY_EXISTS);
     }
-
     // Add the dummy data
     await DummyRepo.addDummy(dummyData,t);
 }
--- src/application/use_cases/dummy/delete_dummy.usecase.ts
+++ src/application/use_cases/dummy/delete_dummy.usecase.ts
@@ -9,12 +9,10 @@ import { DummyRepositoryPort } from "../../port/repositories/dummy_repo.port";
 export const deleteDummyUsecase = async (deleteDummyRepo: DummyRepositoryPort,id: number,t:EntityManager) => {
     // Retrieve dummy data based on the provided ID
     const selectedUser:Dummy[] = await DummyRepo.getDummy(id,t);
-
     // Check if a dummy with the specified ID exists
     if (selectedUser.length) {
         // If it exists, delete the dummy data
         await DummyRepo.deleteDummy(id,t);
-
     } else {
         // If no dummy with the specified ID exists, throw an error
         throw new Error(constants.ERROR_MESSAGE.USER_NOT_FOUND);
